a7481251d357e3bfbbd4b1435e8d244a22baec1c
KMConner <KMConner@users.noreply.github.com>
2020-04-28 02:33:18 +0900

    Add Assignment class

---
+++ PandaLib/Panda/Time.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace PandaLib.Panda
+{
+    public class Time
+    {
+        [DataMember(Name = "time")]
+        public long TickMilliseconds { get; set; }
+
+        [IgnoreDataMember]
+        public DateTimeOffset DateTime
+        {
+            get => DateTimeOffset.FromUnixTimeMilliseconds(TickMilliseconds);
+            set => TickMilliseconds = value.ToUnixTimeMilliseconds();
+        }
+    }
+}
