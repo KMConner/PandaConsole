6ac6a4b2f39dd1220746d291a3d65f5b87ac2907
KMConner <KMConner@users.noreply.github.com>
2020-04-28 17:31:50 +0900

    Add classes related to announcements

---
+++ PandaLib/Panda/AnnouncementCollection.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.Serialization;
+
+namespace PandaLib.Panda
+{
+    [DataContract]
+    class AnnouncementCollection
+    {
+
+        [DataMember(Name = "announcement_collection")]
+        public Announcement[] Announcements { get; set; }
+    }
+}
