f9e514ed1aec87f753c394f9fb4fe8d2ed62bfbc
KMConner <KMConner@users.noreply.github.com>
2020-04-29 14:32:53 +0900

    Implement database sync
    Save ids of announcements after send notification not to notify the same
    announcements again.

---
+++ AnnouncementNotifier/Models/NotifyHistory.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace AnnouncementNotifier.Models
+{
+    class NotifyHistory
+    {
+        [Key]
+        public Guid AnnouncementId { get; set; }
+    }
+}
--- AnnouncementNotifier/Program.cs
+++ AnnouncementNotifier/Program.cs
@@ -4,8 +4,10 @@ using System.Linq;
 using Common.Shell;
 using PandaLib.Panda;
 using HtmlAgilityPack;
+using AnnouncementNotifier.Models;
 using System.Text;
-
+using AnnouncementNotifier.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace AnnouncementNotifier
 {
@@ -13,6 +15,10 @@ namespace AnnouncementNotifier
     {
         static void Main(string[] args)
         {
+            using (var context = new NotifierContext())
+            {
+                context.Database.Migrate();
+            }
             Console.WriteLine("Enter your ECS ID: ");
             string ecsId = Console.ReadLine();
             Console.WriteLine("Enter your password: ");
@@ -29,23 +35,42 @@ namespace AnnouncementNotifier
                 Console.WriteLine(ann.Length);
             }
 
-            foreach (var announce in anns.OrderByDescending(a => a.CreatedAt))
+            using (var context = new NotifierContext())
             {
-                Console.WriteLine($"{announce.Title} -- {announce.SiteTitle}");
-                HtmlDocument doc = new HtmlDocument();
-                doc.LoadHtml(announce.Body);
-                var root = doc.DocumentNode;
-                var sb = new StringBuilder();
-                foreach (var node in root.DescendantsAndSelf())
+                bool changed = false;
+                foreach (var announce in anns.OrderByDescending(a => a.CreatedAt))
                 {
-                    if (!node.HasChildNodes)
+                    if (context.NotifyHistory.AsNoTracking().Any(h => h.AnnouncementId == announce.Id))
                     {
-                        string text = node.InnerText;
-                        if (!string.IsNullOrEmpty(text))
-                            sb.AppendLine(text.Trim());
+                        continue;
                     }
+
+                    Console.WriteLine($"{announce.Title} -- {announce.SiteTitle}");
+                    HtmlDocument doc = new HtmlDocument();
+                    doc.LoadHtml(announce.Body);
+                    var root = doc.DocumentNode;
+                    var sb = new StringBuilder();
+                    foreach (var node in root.DescendantsAndSelf())
+                    {
+                        if (!node.HasChildNodes)
+                        {
+                            string text = node.InnerText;
+                            if (!string.IsNullOrEmpty(text))
+                                sb.AppendLine(text.Trim());
+                        }
+                    }
+                    Console.WriteLine(sb.ToString().Trim());
+                    Console.WriteLine(new string('#', 20));
+                    context.NotifyHistory.Add(new NotifyHistory
+                    {
+                        AnnouncementId = announce.Id,
+                    });
+                    changed = true;
                 }
-                Console.WriteLine(sb.ToString());
+                if (changed)
+                {
+                    context.SaveChanges();
+                }
             }
 
             Console.WriteLine("Hello World!");
