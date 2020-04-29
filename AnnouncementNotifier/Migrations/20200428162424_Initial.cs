20f5651ba7cc74c97dc49038c5382610c2e6cb41
KMConner <KMConner@users.noreply.github.com>
2020-04-29 14:32:53 +0900

    Implement database sync
    Save ids of announcements after send notification not to notify the same
    announcements again.

---
+++ AnnouncementNotifier/Migrations/20200428162424_Initial.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AnnouncementNotifier.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "NotifyHistory",
+                columns: table => new
+                {
+                    AnnouncementId = table.Column<Guid>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_NotifyHistory", x => x.AnnouncementId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "NotifyHistory");
+        }
+    }
+}
