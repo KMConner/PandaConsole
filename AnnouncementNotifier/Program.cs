92919eb017317d8de830084985c0b19aa1ebd627
KMConner <KMConner@users.noreply.github.com>
2020-04-29 17:08:38 +0900

    Organize codes

--- AnnouncementNotifier/Program.cs
+++ AnnouncementNotifier/Program.cs
@@ -1,45 +1,94 @@
-﻿using AnnouncementNotifier.Data;
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using AnnouncementNotifier.Data;
 using AnnouncementNotifier.Models;
 using Common.Shell;
 using HtmlAgilityPack;
 using Microsoft.EntityFrameworkCore;
 using PandaLib.Panda;
 using Slack.Webhooks;
-using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
 
 namespace AnnouncementNotifier
 {
     class Program
     {
+        /// <summary>
+        /// Regular expression for URL.
+        /// </summary>
+        private static readonly Regex urlRegex = new Regex(
+            @"((https?|ftp|file)\://|www.)[A-Za-z0-9\.\-]+(/[A-Za-z0-9\?\&\=;\+!'\(\)\*\-\._~%]*)*",
+            RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
+        /// <summary>
+        /// Parse HTML and convert into plain text.
+        /// </summary>
+        /// <param name="html">HTML to convert from.</param>
+        /// <returns>Plain text.</returns>
+        private static string Html2Text(string html)
+        {
+            HtmlDocument doc = new HtmlDocument();
+            doc.LoadHtml(html);
+            var root = doc.DocumentNode;
+            var sb = new StringBuilder();
+            foreach (var node in root.DescendantsAndSelf())
+            {
+                if (!node.HasChildNodes)
+                {
+                    string text = node.InnerText;
+                    if (!string.IsNullOrEmpty(text))
+                        sb.AppendLine(text.Trim());
+                }
+            }
+            return sb.ToString().Trim();
+        }
+
+        /// <summary>
+        /// Encapsulate urls in <paramref name="txt"/> with &lt; and  &gt;.
+        /// </summary>
+        /// <param name="txt">Text with urls.</param>
+        /// <returns></returns>
+        private static string EncapsulateUrl(string txt)
+        {
+            StringBuilder txtBuilder = new StringBuilder(txt);
+            foreach (var match in urlRegex.Matches(txt).Reverse())
+            {
+                txtBuilder.Insert(match.Index + match.Length, '>');
+                txtBuilder.Insert(match.Index, '<');
+            }
+            return txtBuilder.ToString();
+        }
+
+        private static SakaiUser Signin()
+        {
+
+            Console.Write("Enter your ECS ID: ");
+            string ecsId = Console.ReadLine();
+            string pass = ShellMethods.EnterPassword("Enter your password: ");
+
+            var user = new SakaiUser(ecsId, pass);
+            user.LogIn();
+            return user;
+        }
+
         static void Main(string[] args)
         {
             using (var context = new NotifierContext())
             {
                 context.Database.Migrate();
             }
-            Console.WriteLine("Enter your ECS ID: ");
-            string ecsId = Console.ReadLine();
-            Console.WriteLine("Enter your password: ");
-            string pass = ShellMethods.EnterPassword();
 
-            var user = new SakaiUser(ecsId, pass);
-            user.LogIn();
+            var user = Signin();
             SakaiSiteCollection sites = user.GetSites();
             var anns = new List<Announcement>();
             foreach (var site in sites.Items)
             {
                 var ann = user.GetAnnouncement(site);
                 anns.AddRange(ann);
-                Console.WriteLine(ann.Length);
             }
-
-            string pattern = @"((https?|ftp|file)\://|www.)[A-Za-z0-9\.\-]+(/[A-Za-z0-9\?\&\=;\+!'\(\)\*\-\._~%]*)*";
-            Regex reg = new Regex(pattern, RegexOptions.Compiled | RegexOptions.IgnoreCase);
 
             var client = new SlackClient(ConfigurationManager.AppSettings["SlackWebhookUrl"]);
 
@@ -53,53 +102,34 @@ namespace AnnouncementNotifier
                         continue;
                     }
 
+                    string body = Html2Text(announce.Body);
+                    
+                    // Print to console
                     Console.WriteLine($"{announce.Title} -- {announce.SiteTitle}");
-                    HtmlDocument doc = new HtmlDocument();
-                    doc.LoadHtml(announce.Body);
-                    var root = doc.DocumentNode;
-                    var sb = new StringBuilder();
-                    foreach (var node in root.DescendantsAndSelf())
-                    {
-                        if (!node.HasChildNodes)
-                        {
-                            string text = node.InnerText;
-                            if (!string.IsNullOrEmpty(text))
-                                sb.AppendLine(text.Trim());
-                        }
-                    }
-                    Console.WriteLine(sb.ToString().Trim());
+                    Console.WriteLine(body);
                     Console.WriteLine(new string('#', 20));
                     context.NotifyHistory.Add(new NotifyHistory
                     {
                         AnnouncementId = announce.Id,
                     });
 
-                    string msgText = sb.ToString().Trim();
-                    StringBuilder txtBuilder = new StringBuilder(msgText);
-                    foreach (var match in reg.Matches(msgText).Reverse())
-                    {
-                        txtBuilder.Insert(match.Index + match.Length, '>');
-                        txtBuilder.Insert(match.Index, '<');
-                    }
-
+                    // Send notification to Slack
                     var message = new SlackMessage
                     {
                         IconEmoji = ":panda_face:",
                         Username = announce.SiteTitle,
-                        Text = announce.Title + "\n" + txtBuilder.ToString(),
+                        Text = announce.Title + "\n" + EncapsulateUrl(body),
                     };
-
                     client.Post(message);
 
                     changed = true;
                 }
+
                 if (changed)
                 {
                     context.SaveChanges();
                 }
             }
-
-            Console.WriteLine("Hello World!");
         }
     }
 }
